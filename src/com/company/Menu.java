8021a1c654ee2c1a9b2a8292d5b8362d0bfc014f
wuzupko <wuzupko@gmail.com>
2015-06-18 14:27:16 +0300

    first

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -12,35 +12,79 @@ public class Menu {
 
         ArrayList<Product> productList=new ArrayList<Product>();
 
-        productList.add(new Product("Carrot", 10, 3));
-        productList.add(new Product("Potato", 24, 5));
-        productList.add(new Product("Onion", 56, 2));
-        productList.add(new Product("Tomato", 23, 3));
-        productList.add(new Product("Meat", 20, 3));
-        productList.add(new Product("Becon", 47, 5));
-
+        productList.add(new Product("Carrot", 15, 8));
+        productList.add(new Product("Potato", 20, 5));
+        productList.add(new Product("Onion", 5, 9));
+        productList.add(new Product("Tomato", 10, 10));
+        productList.add(new Product("Meat", 10, 50));
+        productList.add(new Product("Water", 40, 1));
+        productList.add(new Product("Becon", 5, 70));
+        productList.add(new Product("Souce", 0.5, 20));
+        productList.add(new Product("Fruits", 5, 35));
+        productList.add(new Product("Spice", 1, 20));
+        productList.add(new Product("Cheese", 5, 80));
+        productList.add(new Product("Sour", 10, 15));
+        productList.add(new Product("Sugar", 1, 15));
+        productList.add(new Product("Salt", 1, 5));
+        productList.add(new Product("CoffeBeans", 1, 100));
+        productList.add(new Product("TeaLeaves", 1, 80));
+        productList.add(new Product("Beet", 10, 8));
         ArrayList<Dish>  dishList = new ArrayList<Dish>();
 
 
         HashMap<Product, Double> map = new HashMap<Product, Double>();
 
-        map.put(addProduct("Carrot",productList),10.0);
-        map.put(addProduct("Potato",productList),1.0);
-        dishList.add( new Dish("Borsh",  0, map));
+        map.put(addProduct("Carrot",productList),0.05);
+        map.put(addProduct("Tomato",productList),0.05);
+        map.put(addProduct("Potato",productList),0.2);
+        map.put(addProduct("Water",productList),0.3);
+        map.put(addProduct("Onion",productList),0.01);
+        map.put(addProduct("Beet",productList),0.02);
+        map.put(addProduct("Salt",productList),0.0001);
+        dishList.add( new Dish("Borsh",  1, map));
         map.clear();
 
-        map.put(addProduct("Meat",productList),10.0);
-        map.put(addProduct("Becon",productList),10.0);
-        dishList.add( new Dish("Pizza",  1, map));
+        map.put(addProduct("Carrot",productList),0.05);
+        map.put(addProduct("Potato",productList),0.2);
+        map.put(addProduct("Water",productList),0.3);
+        map.put(addProduct("Onion",productList),0.01);
+        map.put(addProduct("Meat",productList),0.1);
+        map.put(addProduct("Salt",productList),0.0001);
+        dishList.add( new Dish("Soup",  1, map));
         map.clear();
 
-        map.put(addProduct("Carrot",productList),10.0);
-        map.put(addProduct("Becon",productList),10.0);
-        dishList.add( new Dish("Tea",  2, map));
+        map.put(addProduct("Meat",productList),0.1);
+        map.put(addProduct("Becon",productList),0.1);
+        map.put(addProduct("Tomato",productList),0.1);
+        map.put(addProduct("Sour",productList),0.4);
+        map.put(addProduct("Cheese",productList),0.2);
+        map.put(addProduct("Spice",productList),0.01);
+        dishList.add( new Dish("Pizza",  2, map));
         map.clear();
 
-        map.put(addProduct("Carrot",productList),10.0);
-        map.put(addProduct("Tomato",productList),10.0);
+        map.put(addProduct("Meat",productList),0.1);
+        map.put(addProduct("Potato",productList),0.1);
+        map.put(addProduct("Sour",productList),0.4);
+        map.put(addProduct("Salt",productList),0.001);
+        dishList.add( new Dish("PotatoWithMeat",  2, map));
+        map.clear();
+        map.put(addProduct("Meat",productList),0.1);
+        map.put(addProduct("Beet",productList),0.1);
+        map.put(addProduct("Tomato",productList),0.1);
+        map.put(addProduct("Salt",productList),0.001);
+        dishList.add( new Dish("Salat",  2, map));
+        map.clear();
+
+
+        map.put(addProduct("Water",productList),0.4);
+        map.put(addProduct("TeaLeaves",productList),0.01);
+        map.put(addProduct("Sugar",productList),0.001);
+        dishList.add( new Dish("Tea",  0, map));
+        map.clear();
+
+        map.put(addProduct("CoffeBeans",productList),0.01);
+        map.put(addProduct("Water",productList),0.2);
+        map.put(addProduct("Sugar",productList),0.001);
         dishList.add( new Dish("Cofee",  0, map));
         map.clear();
 
